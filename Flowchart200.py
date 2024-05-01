3b73d6123b39ca989dd49b2a7ddc39cd5ae6d8d8
ravi-sidharth <thenewravy@gmail.com>
2024-05-01 06:41:55 +0530

    first commit

---
+++ Flowchart200.py
@@ -0,0 +1,103 @@
+# Answer to the Question Number 1:
+
+# num1 = int(input("Enter the first Number:"))
+# num2 = int(input("Enter the second Number:"))
+# print("Total sum is",num1+num2)
+
+# Answer to the Question Number 2:
+
+# num1 = int(input("Enter the first Number:"))
+# num2 = int(input("Enter the second Number:"))
+# average=(num1+num2)/2
+# print(average)
+
+# Answer to the Question Number 4:
+
+# base=float(input("Enter the triange base:"))
+# height=float(input("Enter the triangle height:"))
+# area_of_triangle=(base*height)*1/2
+# print(area_of_triangle)
+
+# Answer to the Question Number 6:
+
+# P = int(input("Enter the invested amount:"))
+# I = int(input("Enter the interest percentage:"))
+# N = int(input("Enter the number of years:"))
+
+# T=  P*(1+I/100)**N
+# print(f"Total amount of I'll have after 'N' years is{T:.2f}")
+
+# Answer to the Question Number 7:
+
+# A=int(input("Enter the first number:"))
+# B=int(input("Enter the second number:"))
+
+# temp =0;
+
+# Two number swapping with using a third value
+# temp=A
+# A=B
+# B=temp;
+
+# print(A,B)
+
+# Answer to the Question Number 8:
+
+# A=int(input("Enter the first number:"))
+# B=int(input("Enter the second number:"))
+
+# Two number swapping without using a third value
+# A=A+B
+# B=A-B
+# A=A-B
+
+# print(A,B)
+
+# Answer to the Question number 9:
+
+# name=str(input("Enter the name:"))
+# print("Welcome",name)
+
+# Answer to the Question number 10:
+
+# A=int(input("Enter the number1:"))
+# B=int(input("Enter the number2:"))
+
+# Sum=A+B
+# Sub=A-B
+# Multiple=A*B
+# Divide=A/B
+
+# print("Sum:",Sum,"\nSub:",Sub,"\nMultiple",Multiple,f"\nDivide:{Divide:.2f}")
+
+# Answer to the Question number 11:
+# length=int(input("Enter the length:"))
+# breadth=int(input("Enter the breadth:"))
+
+# perimeter_of_rectangle=2*(length+breadth)
+# print("Perimeter of rectangle:",perimeter_of_rectangle)
+
+# Answer to the Question number 12:
+# side=int(input("Enter the length:"))
+# perimeter_of_square=4*side
+# area_of_square=side*side
+# print("Perimeter of square:",perimeter_of_square,"\nArea of Square:",area_of_square)
+
+# Answer to the Questionn number 13:
+# side1=int(input("Enter the side1:"))
+# side2=int(input("Enter the side2:"))
+# side3=int(input("Enter the side3:"))
+# semiperimeter_of_triangle=(side1+side2+side3)/2
+# print("semiperimeter of triangle:",semiperimeter_of_triangle)
+
+# Answer to the Question number 14:
+# radius=int(input("Enter the length of circle:"))
+# area_of_circle=3.14*(radius**2)
+# print("Area of circle is",area_of_circle)
+
+# Answer to the Question number 15:
+# circumference=int(input("Enter the circumference:"))
+# diameter=circumference/3.14;
+# print(f"Diameter of Circumference is {diameter:.2f}")
+
+
